e955c1d7911d98054628f7cd504b37d79a118255
DavidBelmonte <david.b.alq@gmail.com>
2024-12-22 11:17:40 +0100

    Version inicial

---
+++ proyectoprueba/src/proyectoprueba/principal.java
@@ -0,0 +1,10 @@
+package proyectoprueba;
+
+public class principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Holi wankenobi");
+
+	}
+}
